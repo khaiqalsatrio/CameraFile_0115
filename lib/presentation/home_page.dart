0f88d6a47cfeca92ccdc2a687b50fb9136e4fe0c
Khaiqol <Khaiqol@gmail.com>
2025-05-16 00:03:08 +0700

    tambah fitur ambil foto di HomePage

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pertemuan7_115/bloc/camera_bloc.dart';
+import 'package:pertemuan7_115/bloc/camera_event.dart';
 import 'package:pertemuan7_115/bloc/camera_state.dart';
 
 class HomePage extends StatelessWidget {
@@ -13,10 +14,31 @@ class HomePage extends StatelessWidget {
       body: SafeArea(
         child: BlocConsumer<CameraBloc, CameraState>(
           listener: (context, state) {
-            if (state is CameraReady && state.snackbarMessage != null) {
+            if (state is Cameraready && state.snackBarMessage != null) {
               ScaffoldMessenger.of(
                 context,
-              ).showSnackBar(SnackBar(content: Text(state.snackbarMessage!)));
+              ).showSnackBar(SnackBar(content: Text(state.snackBarMessage!)));
               context.read<CameraBloc>().add(ClearSnackbar());
             }
           },
+          builder: (context, state) {
+            // final File? imageFile =
+            //    State is CameraReady ? state.imageFile : null;
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton.icon(
+                        icon: const Icon(Icons.camera),
+                        label: const Text('Ambil Foto'),
+                        onPressed: () {
+                          final bloc = context.read<CameraBloc>();
+                          if (bloc.state is! Cameraready) {
+                            bloc.add(InitializeCamera());
+                          }
+                          bloc.add(OpenCameraAndCapture(context));
+                        },
+                      ),
+                    ),
