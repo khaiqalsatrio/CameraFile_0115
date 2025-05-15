2bf13d6c71077df7e060401764466a91cb9ead9c
Khaiqol <Khaiqol@gmail.com>
2025-05-15 23:08:02 +0700

    Menambahkan halaman HomePage dengan permintaan izin kamera dan penyimpanan

---
+++ lib/home_page.dart
@@ -0,0 +1,20 @@
+
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  File? _imageFile;
+
+  Future<void> _requestPermissions() async {
+    await Permission.camera.request();
+    await Permission.storage.request();
+    await Permission.manageExternalStorage.request();
+  }
