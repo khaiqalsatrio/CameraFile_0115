af1656abd34e2ce913fb0d0c89c9fb0261db0d9e
Khaiqol <Khaiqol@gmail.com>
2025-05-15 22:29:09 +0700

    Menambahkan sealed class CameraEvent dengan berbagai jenis event kamera

---
+++ lib/bloc/camera_event.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+sealed class CameraEvent {}
+
+final class InitializeCamera extends CameraEvent {}
+
+final class SwitchCamera extends CameraEvent {}
+
+final class ToogleFlash extends CameraEvent {}
+
+final class TakePicture extends CameraEvent {
+  final void Function(File ImageFile) onPictureTaken;
+  TakePicture(this.onPictureTaken);
+}
