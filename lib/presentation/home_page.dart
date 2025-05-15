be5b4cd1cf3f7d6743ce81c73345faf6b7161c7e
Khaiqol <Khaiqol@gmail.com>
2025-05-15 23:59:21 +0700

    tambah HomePage dengan snackbar dari state CameraBloc

---
+++ lib/presentation/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pertemuan7_115/bloc/camera_bloc.dart';
+import 'package:pertemuan7_115/bloc/camera_state.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Beranda')),
+      body: SafeArea(
+        child: BlocConsumer<CameraBloc, CameraState>(
+          listener: (context, state) {
+            if (state is CameraReady && state.snackbarMessage != null) {
+              ScaffoldMessenger.of(
+                context,
+              ).showSnackBar(SnackBar(content: Text(state.snackbarMessage!)));
+              context.read<CameraBloc>().add(ClearSnackbar());
+            }
+          },
